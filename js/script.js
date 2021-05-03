f4b0913650d955aeaf046d5eeee4ac353c230d9b
Anton Chernichkin <chetoony@gmail.com>
2021-05-03 23:56:23 +0300

    Add doWile and ifelse for qw

--- js/script.js
+++ js/script.js
@@ -1,21 +1,38 @@
 "use strict";
 
-const numberOfFilms = +prompt ('Сколько фильмов вы уже посмотрели?', '');
-const lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
-const lastFilmRaiting = prompt('На сколько оцените его?', '');
-const lastFilmName2 = prompt('Один из последних просмотренных фильмов?', '');
-const lastFilmRaiting2 = prompt('На сколько оцените его?', '');
+let i = 0;
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+
 
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
-    actors: {}, 
+    actors: {},
     genres: [],
     privat: false
 };
 
-personalMovieDB.movies[lastFilmName] = lastFilmRaiting;
-personalMovieDB.movies[lastFilmName2] = lastFilmRaiting2;
+
+do {
+    
+        const lastFilmName = prompt('Один из последних просмотренных фильмов?', '');
+        const lastFilmRaiting = prompt('На сколько оцените его?', '');
+        i++;
+        personalMovieDB.movies[lastFilmName] = lastFilmRaiting;
+}
+while (i < 2);
+
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено довольно мало фильмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30 ) {
+    alert('Вы классический зритель');
+} else {
+    alert('Вы киноман');
+}
+
+// personalMovieDB.movies[lastFilmName2] = lastFilmRaiting2;
 
 console.log(personalMovieDB);
