72b7881c7e7797b38d5c779e8a301ad510d28a63
Anton Chernichkin <chetoony@gmail.com>
2021-05-03 18:19:34 +0300

    Added personalMovieDB

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 "use strict";
 
 let numberOfFilms = prompt ('Сколько фильмов вы уже посмотрели', '');
+let moviesObj = {};
+let actorsObj = {};
+let genresArr = [];
+
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: moviesObj,
+    actors: actorsObj, 
+    genres: genresArr,
+    privat: false
+};
+
